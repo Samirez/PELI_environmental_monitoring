9b72b04d0ccc49b69f00545e855a486d0983a961
herlev <>
2023-10-06 11:08:54 +0200

    add script to bridge uart and mqtt

---
+++ raspberry-pi/uart-mqtt-bridge.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import serial
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("localhost", 1883)
+
+s = serial.Serial("/dev/ttyUSB0", 115200)
+
+while True:
+  line = s.readline().decode("utf-8").strip()
+  print(line)
+  if line.startswith("log:"):
+    client.publish("/log", line.lstrip("log:"))
+  else:
+    if len(line.split(",")) != 3:
+      continue
+    node_id, temperature, humidity = line.split(",")
+    client.publish(f"/node/{node_id}/temperature", temperature)
+    client.publish(f"/node/{node_id}/humidity", humidity)
+  
+client.disconnect()
