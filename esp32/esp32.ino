0cf904284acca212eea0b4266da64c8584f65ce2
herlev <>
2023-10-06 10:14:16 +0200

    mesh network reading sensor values and sending it to specified node

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,36 +1,90 @@
+#include "Arduino.h"
 #include <Wire.h>
-
 #include <SparkFun_Qwiic_Humidity_AHT20.h>
+#include "painlessMesh.h"
+#include "secrets.h"
+
+#define   MESH_PORT       5555
+
 AHT20 humiditySensor;
+Scheduler userScheduler; // to control your personal task
+painlessMesh mesh;
+
+#define FORWARDER 3859401677
+
+void sendMessage() ; // Prototype so PlatformIO doesn't complain
+
+Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
+
+void sendMessage() {
+  if (humiditySensor.available() == true) {
+    float temperature = humiditySensor.getTemperature();
+    float humidity = humiditySensor.getHumidity();
+    String msg = "";
+    msg += temperature;
+    msg += ",";
+    msg += humidity;
+    if (mesh.getNodeId() != FORWARDER) {
+      Serial.println(msg);
+      Serial.print("Sending message to ");
+      Serial.println(FORWARDER);
+      mesh.sendSingle(FORWARDER, msg);
+    } else {
+      Serial.print(mesh.getNodeId());
+      Serial.print(",");
+      Serial.println(msg);
+    }
+  }
+
+  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+}
+
+void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("%u,%s\n", from, msg.c_str());
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Qwiic Humidity AHT20 examples");
-
   Wire.begin(2, 3);
 
   if (humiditySensor.begin() == false) {
     Serial.println("AHT20 not detected. Please check wiring. Freezing.");
     while (1);
   }
-  Serial.println("AHT20 acknowledged.");
+
+  //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  Serial.print("Node id: ");
+  Serial.println(mesh.getNodeId());
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
 }
 
 void loop()
 {
-  if (humiditySensor.available() == true) {
-    float temperature = humiditySensor.getTemperature();
-    float humidity = humiditySensor.getHumidity();
-
-    Serial.print("Temperature: ");
-    Serial.print(temperature, 2);
-    Serial.print(" C\t");
-    Serial.print("Humidity: ");
-    Serial.print(humidity, 2);
-    Serial.print("% RH");
-
-    Serial.println();
-  }
-
-  delay(2000);
+  mesh.update();
 }
+
+
+// white cable: 3859401677
+// black cable: 3859403313
