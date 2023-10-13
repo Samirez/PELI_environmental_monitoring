4f3a717346b64cdfd80a15336bad02879a60e5ca
ahnai17 <ahnai17@student.sdu.dk>
2023-10-13 14:00:44 +0200

    edited home view to access the sensor readings from table

--- Django_ui/forest_live_monitoring/humidity/models.py
+++ Django_ui/forest_live_monitoring/humidity/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class sensors(models.Model):
+    Node_id = models.AutoField(primary_key=True)
+    Latitude = models.DecimalField(max_digits=8, decimal_places=6)
+    Longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    class Meta:
+        db_table = 'sensors'
+        unique_together = (('Latitude', 'Longitude'),)
+
+class sensorReadings(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Node_id = models.ForeignKey(sensors, on_delete=models.CASCADE)
+    Temperature = models.FloatField()
+    Humidity = models.FloatField()
+    Timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'sensorReadings'
+        unique_together = (('ID', 'Node_id'),)
+
--- Django_ui/forest_live_monitoring/humidity/views.py
+++ Django_ui/forest_live_monitoring/humidity/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from humidity.models import sensorReadings, sensors
 
-# Create your views here.
 def home_view(request):
-    return render(request, 'home.html', {})
+    # Get the latest sensor reading
+    latest_reading = sensorReadings.objects.latest('Timestamp')
+    
+    # Get the temperature and humidity values from the latest reading
+    temperature = latest_reading.Temperature
+    humidity = latest_reading.Humidity
+    
+    # Render the template with the temperature and humidity values
+    return render(request, 'home.html', {'temperature': temperature, 'humidity': humidity})
+     
