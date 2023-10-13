27610b3582c6aed20e0046957e23f8b7ec302a99
ahnai17 <ahnai17@student.sdu.dk>
2023-10-13 14:10:45 +0200

    display dummy data on UI

--- Django_ui/forest_live_monitoring/humidity/views.py
+++ Django_ui/forest_live_monitoring/humidity/views.py
@@ -4,11 +4,10 @@ from humidity.models import sensorReadings, sensors
 def home_view(request):
     # Get the latest sensor reading
     latest_reading = sensorReadings.objects.latest('Timestamp')
-    
-    # Get the temperature and humidity values from the latest reading
-    temperature = latest_reading.Temperature
-    humidity = latest_reading.Humidity
+    context = {
+        'latest_reading': latest_reading
+    }
     
     # Render the template with the temperature and humidity values
-    return render(request, 'home.html', {'temperature': temperature, 'humidity': humidity})
+    return render(request, 'home.html', context)
      
