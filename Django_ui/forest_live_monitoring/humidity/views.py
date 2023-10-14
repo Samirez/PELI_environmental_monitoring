20c845a4a6d64fbda904b5869ec214f8db22cb1c
ahnai17 <ahnai17@student.sdu.dk>
2023-10-14 15:15:40 +0200

    JSON view function implemented
    JSON is being read every second from the database that is then sent to the frontend through jQuery in JavaScript

--- Django_ui/forest_live_monitoring/humidity/views.py
+++ Django_ui/forest_live_monitoring/humidity/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
-from humidity.models import sensorReadings, sensors
+from .models import sensorReadings, sensors
+from django.http import JsonResponse
 
 def home_view(request):
     # Get the latest sensor reading
-    latest_reading = sensorReadings.objects.latest('Timestamp')
+    sensors_with_latest_readings = []
+    # get all sensors
+    all_sensors = sensors.objects.all()
+
+    for sensor in all_sensors:
+        # Retrieve the latest reading for each sensor
+        latest_reading = sensorReadings.objects.filter(Node_id=sensor).latest('Timestamp')
+
+        # Append the sensor and its latest reading to the list
+        sensors_with_latest_readings.append((sensor, latest_reading))
+
     context = {
-        'latest_reading': latest_reading
+        'sensors_with_latest_readings': sensors_with_latest_readings
     }
     
-    # Render the template with the temperature and humidity values
+    # Render the template with the sensor readings
     return render(request, 'home.html', context)
+
+# create JSON response for the latest readings
+def latest_readings(request):
+    latest_readings = sensorReadings.objects.all().order_by('-Timestamp')[:4]
+
+    data = []
+    for reading in latest_readings:
+        data.append({
+            'Temperature': reading.Temperature,
+            'Humidity': reading.Humidity,
+            'Location': f'{reading.Node_id.Latitude}, {reading.Node_id.Longitude}',
+        })
+
+    return JsonResponse({'latest_readings': data})
      
