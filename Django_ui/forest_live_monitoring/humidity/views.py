ad74095e6da50f4787dd30a02d3836a9ee10f961
ahnai17 <ahnai17@student.sdu.dk>
2023-11-03 10:31:23 +0100

    views added views back

--- Django_ui/forest_live_monitoring/humidity/views.py
+++ Django_ui/forest_live_monitoring/humidity/views.py
@@ -2,30 +2,27 @@ from django.shortcuts import render
 from .models import sensorReadings, sensors
 from django.http import JsonResponse
 
+# Create your views here.
 def home_view(request):
     # Get the latest sensor reading
+    latest_reading = sensorReadings.objects.latest('Timestamp')
     sensors_with_latest_readings = []
     # get all sensors
     all_sensors = sensors.objects.all()
-
     for sensor in all_sensors:
         # Retrieve the latest reading for each sensor
         latest_reading = sensorReadings.objects.filter(Node_id=sensor).latest('Timestamp')
-
         # Append the sensor and its latest reading to the list
         sensors_with_latest_readings.append((sensor, latest_reading))
-
     context = {
+        'latest_reading': latest_reading,
         'sensors_with_latest_readings': sensors_with_latest_readings
     }
-    
-    # Render the template with the sensor readings
     return render(request, 'home.html', context)
 
-# create JSON response for the latest readings
+
 def latest_readings(request):
     latest_readings = sensorReadings.objects.all().order_by('-Timestamp')[:4]
-
     data = []
     for reading in latest_readings:
         data.append({
@@ -33,6 +30,4 @@ def latest_readings(request):
             'Humidity': reading.Humidity,
             'Location': f'{reading.Node_id.Latitude}, {reading.Node_id.Longitude}',
         })
-
     return JsonResponse({'latest_readings': data})
-     
