dfdc6f2519d1a86fabb82de2e957c87296a904d1
ahnai17 <ahnai17@student.sdu.dk>
2023-11-17 09:21:37 +0100

    Update tests.py

--- Django_ui/forest_live_monitoring/humidity/tests.py
+++ Django_ui/forest_live_monitoring/humidity/tests.py
@@ -1,12 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
 class HumidityTest(TestCase):
     """ Tests for the application views """
     def test_humidity(self):
         """ Test the humidity view """
-        response = self.client.get('/humidity/')
+        url = '/humidity/'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Humidity')
         self.assertTemplateUsed(response, 'humidity/humidity.html')
-        
+
+    def test_humidity_data(self):
+        """ Test the humidity data view """
+        url = '/humidity/data/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Humidity')
+        self.assertTemplateUsed(response, 'humidity/humidity_data.html')
