734e73cbf7291df7416e22aeeae68136fd73ed28
ahnai17 <ahnai17@student.sdu.dk>
2023-11-17 10:45:40 +0100

    added second test for JSON reading of sensor data

--- Django_ui/forest_live_monitoring/humidity/tests.py
+++ Django_ui/forest_live_monitoring/humidity/tests.py
@@ -10,6 +10,8 @@ class HumidityTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertContains(response, '')
-    
-
-
+    def test_latest_readings(self):
+        """ Test the latest_readings view """
+        response = self.client.get(reverse('latest_readings'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'latest_readings')
