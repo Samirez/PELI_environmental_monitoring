1948f7fdd473d17dd6fe6f24fb9b42e8d160208e
ahnai17 <ahnai17@student.sdu.dk>
2023-11-17 10:16:06 +0100

    home view test implemented

--- Django_ui/forest_live_monitoring/humidity/tests.py
+++ Django_ui/forest_live_monitoring/humidity/tests.py
@@ -1,23 +1,15 @@
 from django.test import TestCase
 from django.urls import reverse
 import humidity.views
-from django.contrib import admin
-
-
-class Admin_test(TestCase):
-    """ Tests for the application views """
-    def admin_test(self):
-        """ Test the admin view """
-        response = self.client.get(reverse('admin'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'admin')
-        self.assertTemplateUsed(response, admin.site.urls)
 
 class HumidityTest(TestCase):
     """ Tests for the application views """
-    def humidity_test(self):
+    def test_humdity(self):
         """ Test the humidity view """
-        response = self.client.get(reverse('humidity'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'latest_readings')
         self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, '')
+    
+
+
